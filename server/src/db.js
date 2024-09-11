853e1e781f8af6f0216d21f65d74653f30de803f
Javier <pardosblesajavier@gmail.com>
2024-09-11 20:31:52 +0200

    Backend: refactor local db name

--- server/src/db.js
+++ server/src/db.js
@@ -1,14 +1,13 @@
 import { createClient } from "@libsql/client";
 
 // load the env vars to connect to turso
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 /**
  * @description Connect to the turso database
  */
 export const tursoDB = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? "file:./../local.db",
+  url: process.env.TURSO_DATABASE_URL ?? "file:./../mini-link-local.db",
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
-
