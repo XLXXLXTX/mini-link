1ebdea702cd03d11655b0859c4f50b710a13c7c5
Javier <pardosblesajavier@gmail.com>
2024-09-16 19:20:03 +0200

    Backend: set the name of the local db based on the field from the .env file.

--- server/src/db.js
+++ server/src/db.js
@@ -1,13 +1,13 @@
-import { createClient } from "@libsql/client";
+import { createClient } from '@libsql/client';
 
 // load the env vars to connect to turso
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 /**
  * @description Connect to the turso database
  */
 export const tursoDB = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? "file:./../mini-link-local.db",
+  url: process.env.TURSO_DATABASE_URL ?? `file:${process.env.LOCAL_DB_PATH}`,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
