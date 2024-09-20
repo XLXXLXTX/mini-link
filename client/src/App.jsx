a4be31955e95b14c955bbef8664a76b1f38f6bb6
Javier <pardosblesajavier@gmail.com>
2024-09-20 13:53:07 +0200

    Frontend: import fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import URLShortenerView from './views/URLShortenerView';
 import LoginView from './views/LoginView';
